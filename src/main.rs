f8d66dbfdace32456291edd2aba8f886411d9e96
Matt Monsour <mzmonsour@gmail.com>
2015-01-22 19:35:49 -0500

    Implement 16-bit address space

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct AddressSpace {
+    data: [u8; 0xFFFF],
+}
+
+impl AddressSpace {
+
+    fn new() -> AddressSpace {
+        AddressSpace { data: [0; 0xFFFF] }
+    }
+
+    fn read(&self, addr: u16) -> u8 {
+        self.data[addr as usize]
+    }
+
+    fn write(&mut self, addr: u16, data: u8) {
+        self.data[addr as usize] = data;
+    }
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
