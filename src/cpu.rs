3c57a2b57a67f4aa76fe78a7d32848eadfa7276b
Matt Monsour <mzmonsour@gmail.com>
2015-11-15 01:18:06 -0500

    Fix opcodes for BIT instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -1085,42 +1085,42 @@ impl Cpu {
                         self.ram.write(addr, shift);
                     },
                     // Test bit
-                    0x47 => {
+                    0x47 | 0x4F | 0x57 | 0x5F | 0x67 | 0x6F | 0x77 | 0x7F => {
                         let x = self.reg.read(Register::A);
                         let b = (subop & 0x38) >> 3;
                         self.test_bit(x, b);
                     },
-                    0x40 => {
+                    0x40 | 0x48 | 0x50 | 0x58 | 0x60 | 0x68 | 0x70 | 0x78 => {
                         let x = self.reg.read(Register::B);
                         let b = (subop & 0x38) >> 3;
                         self.test_bit(x, b);
                     },
-                    0x41 => {
+                    0x41 | 0x49 | 0x51 | 0x59 | 0x61 | 0x69 | 0x71 | 0x79 => {
                         let x = self.reg.read(Register::C);
                         let b = (subop & 0x38) >> 3;
                         self.test_bit(x, b);
                     },
-                    0x42 => {
+                    0x42 | 0x4A | 0x52 | 0x5A | 0x62 | 0x6A | 0x72 | 0x7A => {
                         let x = self.reg.read(Register::D);
                         let b = (subop & 0x38) >> 3;
                         self.test_bit(x, b);
                     },
-                    0x43 => {
+                    0x43 | 0x4B | 0x53 | 0x5B | 0x63 | 0x6B | 0x73 | 0x7B => {
                         let x = self.reg.read(Register::E);
                         let b = (subop & 0x38) >> 3;
                         self.test_bit(x, b);
                     },
-                    0x44 => {
+                    0x44 | 0x4C | 0x54 | 0x5C | 0x64 | 0x6C | 0x74 | 0x7C => {
                         let x = self.reg.read(Register::H);
                         let b = (subop & 0x38) >> 3;
                         self.test_bit(x, b);
                     },
-                    0x45 => {
+                    0x45 | 0x4D | 0x55 | 0x5D | 0x65 | 0x6D | 0x75 | 0x7D => {
                         let x = self.reg.read(Register::L);
                         let b = (subop & 0x38) >> 3;
                         self.test_bit(x, b);
                     },
-                    0x46 => {
+                    0x46 | 0x4E | 0x56 | 0x5E | 0x66 | 0x6E | 0x76 | 0x7E => {
                         let addr = self.reg.read_u16(Register::HL);
                         let x = self.ram.read(addr);
                         let b = (subop & 0x38) >> 3;
